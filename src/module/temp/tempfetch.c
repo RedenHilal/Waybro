a843ef26fc0ef55d8e2aaffe4cfd662e6a4348b4
Hilal Rahmatullah <hilalrahmatullah11@gmail.com>
2026-08-16 20:04:32 +0700

    module: add temperature module

--- src/module/temp/tempfetch.c
+++ src/module/temp/tempfetch.c
@@ -1,30 +1,118 @@
-#include "fetcher.h"
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdint.h>
+#include <fcntl.h>
+#include <sys/timerfd.h>
 
-int get_temp_fd(int it_sec);
-void temp_get(struct fd_object * object);
-void temp_handle(struct epoll_event * event);
+#include "widget.h"
+#include "module.h"
+#include "macro.h"
+#include "style.h"
 
-static const struct module_interface mod = {
+int get_temp_fd(struct wb_context * ctx);
+void parse_temp_sty(struct wb_config_setting * set, struct wb_style_main * msty,
+				struct wb_style_base * base);
+void * set_up_temp(struct wb_context * ctx);
+void temp_handle(struct wb_event * event, struct wb_context * ctx, void * data);
+void render_temp(struct wb_context * ctx, void * data);
+
+#define TEMP_PATH "/sys/class/thermal/thermal_zone0/temp"
+
+static struct module_interface mod = {
 	.module_name	= "temperature",
-	.parse_sty		= NULL,
+	.parse_sty		= parse_temp_sty,
 	.get_fd			= get_temp_fd,
-	.set_up			= temp_get,
+	.set_up			= set_up_temp,
 	.handle_event	= temp_handle,
-	.hanlde_update	= NULL,
+	.emit_layout	= render_temp,
 	.clean_up		= NULL
+};
+
+struct temp_state {
+	int temp_now;
+	int temp_fd;
+
+	char text[64];
+};
+
+struct temp_setting {
+	int interval;
+};
+
+static const struct config_dispatch temp_config[] = {
+	{
+		.field_name = "interval",
+		.default_int = 5,
+		.field_type = WB_STYLE_INT,
+		.offset = offsetof(struct temp_setting, interval)
+	}
+};
+
+static const struct wb_widget_callback temp_cb = {
+
 };
 
 struct module_interface * mod_init(){
 	return &mod;
 }
 
-int get_temp_fd(int pipe){
-	Thread_struct * param = data;
-	struct temp_style * style = param->styles[TEMP];
-	int it_sec = style->it_sec;
+static void
+render_text(struct wb_context * ctx, void * data)
+{
+	const struct wb_public_api * api = mod.api;
+	struct temp_state * state = data;
+
+	struct wb_widget_text_data text = api->widget->default_text(ctx);
+	text.string = state->text;
+
+	api->widget->text(ctx, &text);
+}
+
+void
+render_temp(struct wb_context * ctx, void * data)
+{
+	const struct wb_public_api * api = mod.api;
+	struct temp_state * state = data;
+	static int id = -1;
+
+	if (id < 0) {
+		id = api->widget->allocate_id(ctx);
+		api->widget->set_id(ctx, id, state, WB_POINTER_HOVER, &temp_cb);
+	}
+
+	int event = api->widget->get_event(ctx, id);
+
+	struct wb_widget_rect_special rect = {
+		.rect = api->widget->default_rect(ctx, event)
+	};
+
+	rect.rect.data = state;
+	rect.rect.child_cb = render_text;
+
+	api->widget->bind_id(ctx, id, &rect);
+	api->widget->rect_special(ctx, &rect);
+}
+
+void
+parse_temp_sty(struct wb_config_setting * set, struct wb_style_main * msty,
+				struct wb_style_base * base)
+{
+	const struct wb_public_api * api = mod.api;
+	struct temp_setting * setting = malloc(sizeof(struct temp_setting));
+
+	int length = sizeof(temp_config)/sizeof(temp_config[0]);
+	api->config->parse_config(temp_config, length, setting, set);
+
+	mod.custom_style = setting;
+}
+
+int
+get_temp_fd(struct wb_context * ctx)
+{
+	struct temp_setting * setting = mod.custom_style;
+	int it_sec = setting->interval;
 
     int time_fd = timerfd_create(CLOCK_REALTIME, 0);
-    set_nonblock(time_fd);
 
     struct itimerspec timer = {0};
     timer.it_value.tv_sec = it_sec;
@@ -35,25 +123,40 @@ int get_temp_fd(int pipe){
     return time_fd;
 }
 
-void temp_handle(struct wb_event * event, struct wb_context * ctx){
-	struct fd_object * object = event.data.ptr;
-	temp_get(ctx);
+static void
+temp_get(struct temp_state * state)
+{
+	const struct wb_public_api * api = mod.api;
+    char buffer[16];
+
+	lseek(state->temp_fd, 0, SEEK_SET);
+    read(state->temp_fd, buffer, sizeof(buffer));
+
+    state->temp_now = atoi(buffer) / 1000;
+	api->mod->sub_text(mod.base_style->format, "temperature", state->text,
+					&state->temp_now, WB_MOD_INT, 64);
 }
 
-void temp_get(struct wb_context * ctx){
-    int temp;
-    char buffer[128], drainbuff[128];
-    FILE * temp_fd = fopen(TEMP_PATH, "r");
+void *
+set_up_temp(struct wb_context * ctx)
+{
+	struct temp_state * state = malloc(sizeof(struct temp_state));
+	struct temp_setting * setting = mod.custom_style;
+	int fd = open(TEMP_PATH, O_RDONLY | O_CLOEXEC);
 
-    read(object->fd, drainbuff, sizeof(drainbuff));
+	state->temp_fd = fd;
+	temp_get(state);
 
-    fgets(buffer,sizeof(buffer),temp_fd);
-    fseek(temp_fd, 0, SEEK_SET);
+	return state;
+}
 
-    temp = atoi(buffer) / 1000;
+void
+temp_handle(struct wb_event * event, struct wb_context * ctx, void * data)
+{
+	const struct wb_public_api * api = mod.api;
+	uint64_t count;
+	read(event->fd, &count, sizeof(uint64_t));
 
-    fclose(temp_fd);
-
-    write(object->pipe,&(Event){TEMP,1,temp},sizeof(Event));
-
+	temp_get(data);
+	api->mod->trigger_update(ctx);
 }
